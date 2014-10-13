81bd068046df87b8d98eb40ed3314d8094f801e4
Henning Günther <guenther@forsyte.at>
2014-10-13 17:19:25 +0200

    Also rework SMTPool implementation

--- SMTPool.hs
+++ SMTPool.hs
@@ -5,30 +5,52 @@ import Language.SMTLib2
 import Language.SMTLib2.Connection
 
 import Data.Pool
+import Control.Exception
 
-data SMTInstance a = forall b. SMTBackend b IO =>
-                     SMTInstance { instanceConn :: SMTConnection b
-                                 , instanceVars :: a
-                                 }
+data SMTInstance info a = forall b. SMTBackend b IO =>
+                          SMTInstance { instanceConn :: SMTConnection b
+                                      , instanceVars :: a
+                                      , instanceInfo :: info
+                                      }
 
-type SMTPool a = Pool (SMTInstance a)
+type SMTPool info a = Pool (SMTInstance info a)
 
 createSMTPool :: SMTBackend b IO
                  => IO b
                  -> SMT a
-                 -> IO (SMTPool a)
-createSMTPool createBackend act
+                 -> IO (SMTPool () a)
+createSMTPool backend act = createSMTPool' backend () act
+
+createSMTPool' :: SMTBackend b IO
+                 => IO b
+                 -> info
+                 -> SMT a
+                 -> IO (SMTPool info a)
+createSMTPool' createBackend info act
   = createPool (do
                    b <- createBackend
                    conn <- open b
                    vars <- performSMTExitCleanly conn act
-                   return $ SMTInstance conn vars)
+                   return $ SMTInstance conn vars info)
     (\(SMTInstance { instanceConn = conn }) -> close conn)
     1 5 10
 
-withSMTPool :: SMTPool a -> (a -> SMT b) -> IO b
+withSMTPool :: SMTPool info a -> (a -> SMT b) -> IO b
 withSMTPool pool act
-  = withResource pool $
-    \(SMTInstance { instanceConn = conn
-                  , instanceVars = vars })
-    -> performSMTExitCleanly conn (act vars)
+  = withSMTPool' pool (\info vars -> do
+                          res <- act vars
+                          return (res,info))
+
+withSMTPool' :: SMTPool info a -> (info -> a -> SMT (b,info)) -> IO b
+withSMTPool' pool act = do
+  (inst@SMTInstance { instanceConn = conn
+                    , instanceVars = vars
+                    , instanceInfo = info },local) <- takeResource pool
+  (do
+      (res,info') <- performSMTExitCleanly conn
+                     (act info vars)
+      putResource local (SMTInstance { instanceConn = conn
+                                     , instanceVars = vars
+                                     , instanceInfo = info' })
+      return res)
+    `onException` (destroyResource pool local inst)
