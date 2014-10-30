2275d606f46bd23bba27463b2fd177d3d94caaa6
Henning Günther <guenther@forsyte.at>
2014-10-30 09:56:52 +0100

    Add new command-line parameters to benchmark suite

--- test/Benchmarks.hs
+++ test/Benchmarks.hs
@@ -8,8 +8,12 @@ main = do
   defaultMainWithBechmarks
        [Benchmark { target = "../ic3.cabal"
                   , cmdargs = ["-t",show (to+5)++"s"]
-                  , configs = Or [Set (Variant name) (RuntimeParam $ name++".bc")
-                                 | name <- lines benchmarks ]
+                  , configs = And [Or [Set (Variant name) (RuntimeParam $ name++".bc")
+                                      | name <- lines benchmarks ]
+                                  ,Or [Set NoMeaning (RuntimeParam "-E monolithic")
+                                      ,Set NoMeaning (RuntimeParam "-E blockwise")]
+                                  ,Or [Set NoMeaning (RuntimeParam "-O")
+                                      ,Set NoMeaning (RuntimeParam "")]]
                   , progname = Just "hctigar"
                   , benchTimeOut = Just (fromIntegral to)
                   }
