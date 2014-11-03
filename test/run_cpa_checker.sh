e3b19842a376abcf720a79e3b76a70fde600dc71
Henning Günther <guenther@forsyte.at>
2014-11-03 16:01:38 +0100

    Fix CPAChecker script

--- test/run_cpa_checker.sh
+++ test/run_cpa_checker.sh
@@ -4,8 +4,8 @@ for b in $(cat benchmarks);
 do
     echo -n $b
     echo -n ' '
-    timeout 500s /usr/bin/time -f 'RUNTIME: %e' cpa.sh -sv-comp13--combinations-predicate $b.cpachecker.c &> tmpresults
-    if grep 'Verification result: TRUE' tmpresults
+    timeout 500s /usr/bin/time -f '%e' cpa.sh -sv-comp13--combinations-predicate $b.cpachecker.c &> tmpresults
+    if grep 'Verification result: TRUE' tmpresults > /dev/null
     then
 	t=$(tail -n 1 tmpresults)
 	echo $t
