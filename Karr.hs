01a92dafb7d558e55229d80aa5a5b96074db518f
Henning Günther <guenther@forsyte.at>
2014-11-24 14:51:40 +0100

    Fix Karr implementation

--- Karr.hs
+++ Karr.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE ScopedTypeVariables,GADTs #-}
 module Karr where
 
-import Affine
+{-import Affine
 import Polynom
 import ExprPreprocess
-import Model
+import Model-}
 
 import Language.SMTLib2
 import Language.SMTLib2.Internals
 
-import Data.Vector as Vec
+import Data.Vector (Vector,(//),(!))
+import qualified Data.Vector as Vec
 import Linear.Vector
 import Linear.Matrix ((!*))
 import Prelude as P hiding (all)
@@ -23,8 +24,6 @@ import Data.Maybe (catMaybes)
 import Data.List as L
 
 import Data.Graph.Inductive as Gr
-
-import Debug.Trace
 
 data DiagonalBasis = DiagBasis { basisVector :: !(Vector Integer)
                                , basisVectors :: !(IntMap (Vector Integer))
@@ -86,27 +85,28 @@ finishKarr st = if karrComplete st
                 then st
                 else finishKarr (stepKarr st)
 
-initKarr :: Int -- ^ The total number of states
+initKarr :: Int -- ^ The number of variables
          -> Int -- ^ The initial state
          -> (Int -> Int -> [(Vector (Vector Integer),Vector Integer)])
          -> (Int -> [Int])
          -> KarrState
-initKarr numSt initSt trans succ
+initKarr numVar initSt trans succ
   = KarrState { karrNodes = IMap.singleton initSt
-                            (DiagBasis { basisVector = Vec.replicate numSt 0
+                            (DiagBasis { basisVector = Vec.replicate numVar 0
                                        , basisVectors = IMap.fromList
-                                                        [ (i,Vec.generate numSt
+                                                        [ (i,Vec.generate numVar
                                                              (\i' -> if i==i'
                                                                      then 1
                                                                      else 0))
-                                                        | i <- [0..(numSt-1)] ]
+                                                        | i <- [0..(numVar-1)] ]
                                        })
               , karrTransition = trans
               , karrSuccessor = succ
-              , karrQueue = [ (initSt,Vec.generate numSt (\i' -> if i==i'
-                                                                 then 1
-                                                                 else 0))
-                            | i <- [0..(numSt-1)] ] }
+              , karrQueue = (initSt,Vec.generate numVar (const 0))
+                            :[ (initSt,Vec.generate numVar (\i' -> if i==i'
+                                                                   then 1
+                                                                   else 0))
+                             | i <- [0..(numVar-1)] ] }
 
 stepKarr :: KarrState -> KarrState
 stepKarr st = case karrQueue st of
@@ -195,9 +195,66 @@ updateBasis t basis
                         rx = x' ^-^ xi'
                     in reduceVec rx xs
 
-extractPredicateVec :: DiagonalBasis -> [Vector Integer]
+extractPredicateVec :: DiagonalBasis -> [(Vector Integer,Integer)]
 extractPredicateVec diag
-  = [ Vec.generate ((Vec.length nbase)+1)
+  = [ (Vec.slice (numBasis+1) numVars line,line!numBasis)
+    | line <- Vec.toList solved
+    , Vec.all (==0) $ Vec.slice 0 numBasis line]
+  where
+    nbasis = IMap.fromList $ zip [0..] $ IMap.elems (basisVectors diag)
+    numBasis = IMap.size nbasis
+    numVars = Vec.length (basisVector diag)
+    system = Vec.generate numVars
+             (\i -> Vec.generate (numBasis+1+numVars)
+                    (\j -> if j < numBasis
+                           then (nbasis IMap.! j)!i
+                           else if j==numBasis
+                                then (basisVector diag)!i
+                                else if i==j-numBasis-1
+                                     then 1
+                                     else 0))
+    solved = gaussElim system
+extractPredicateVec diag
+  = elimLines (IMap.keys (basisVectors diag)) matr diag
+  where
+    matr = Vec.generate (Vec.length $ basisVector diag)
+           (\i -> Vec.generate (Vec.length $ basisVector diag)
+                  (\j -> if i==j then 1 else 0))
+    elimLines [] matr diag = [(matr!0,(basisVector diag)!0)]
+    elimLines (i:is) matr diag = let (nmatr,ndiag) = elimLine i matr diag
+                                 in elimLines is nmatr ndiag
+    elimLine :: Int -> Vector (Vector Integer) -> DiagonalBasis -> (Vector (Vector Integer),
+                                                                    DiagonalBasis)
+    elimLine i matr diag
+      = let col = (basisVectors diag) IMap.! i
+            piv = col!i
+            ms = fmap (\el -> let p = lcm piv el
+                              in (p `div` el,p `div` piv)
+                      ) col
+        in (Vec.imap (\j ln -> if col!j==0
+                               then ln
+                               else (let (m,n) = ms!j
+                                     in ln^*m ^-^ (matr!i)^*n)
+                     ) matr,
+            DiagBasis { basisVector = Vec.imap
+                                      (\j c -> if col!j==0
+                                               then c
+                                               else (let (m,n) = ms!j
+                                                     in c*m-(basisVector diag)!i*n)
+                                      ) (basisVector diag)
+                      , basisVectors = IMap.mapWithKey
+                                       (\p vec
+                                        -> Vec.imap
+                                           (\j v -> if col!j==0
+                                                    then v
+                                                    else (let (m,n) = ms!j
+                                                          in v*m-(vec!i)*n)
+                                           ) vec
+                                       ) (basisVectors diag)
+                      })
+{-extractPredicateVec diag
+  = trace (show (diag,f,nbase,ndiag)) $
+    [ Vec.generate ((Vec.length nbase)+1)
       (\i' -> if i'==0
               then -v
               else (if i'==(i+1)
@@ -217,7 +274,7 @@ extractPredicateVec diag
                                                nf = lcm (m `div` (cbase!i)) cf
                                            in (nf,base0 ^-^ base1)
                                   ) (1,basisVector diag) (basisVectors diag)
-    ndiag = IMap.mapWithKey (\i v -> fmap (\x -> (x*f) `div` (v!i)) v) (basisVectors diag)
+    ndiag = IMap.mapWithKey (\i v -> fmap (\x -> (x*f) `div` (v!i)) v) (basisVectors diag)-}
 
 predicateVecToFun :: (Args st) => Vector Integer -> st -> SMTExpr Bool
 predicateVecToFun v st = (constant c) .==. (app plus $ catMaybes $ match (fromArgs st) facs)
@@ -234,3 +291,66 @@ predicateVecToFun v st = (constant c) .==. (app plus $ catMaybes $ match (fromAr
 
 testExpr :: SMTExpr Integer
 testExpr = 2*((Var 0 ()) + (constant 4)) + (Var 1 ()) + (Var 0 ())*(Var 0 ())
+
+testKarr1 :: [(Int,(Vector Integer,Integer))]
+testKarr1 = [ (n,vec)
+            | (n,basis) <- IMap.toList (karrNodes st')
+            , vec <- extractPredicateVec basis ]
+  where
+    st = initKarr 2 0
+         (\f t -> case (f,t) of
+                        (0,1) -> [(Vec.fromList [Vec.fromList [0,0]
+                                                ,Vec.fromList [0,0]],
+                                   Vec.fromList [0,0])]
+                        (1,2) -> [(Vec.fromList [Vec.fromList [1,0]
+                                                ,Vec.fromList [0,1]],
+                                   Vec.fromList [0,0])]
+                        (1,3) -> [(Vec.fromList [Vec.fromList [1,0]
+                                                ,Vec.fromList [0,1]],
+                                   Vec.fromList [0,0])]
+                        (2,1) -> [(Vec.fromList [Vec.fromList [1,0]
+                                                ,Vec.fromList [0,1]],
+                                   Vec.fromList [4,1])]
+                        (3,4) -> [(Vec.fromList [Vec.fromList [1,0]
+                                                ,Vec.fromList [0,1]],
+                                   Vec.fromList [0,0])]
+                        (4,3) -> [(Vec.fromList [Vec.fromList [1,0]
+                                                ,Vec.fromList [0,1]],
+                                   Vec.fromList [-4,-1])]
+                        (3,5) -> [(Vec.fromList [Vec.fromList [1,0]
+                                                ,Vec.fromList [0,1]],
+                                   Vec.fromList [0,0])]
+                        (5,5) -> [(Vec.fromList [Vec.fromList [1,0]
+                                                ,Vec.fromList [0,1]],
+                                   Vec.fromList [0,0])])
+         (\f -> case f of
+           0 -> [1]
+           1 -> [2,3]
+           2 -> [1]
+           3 -> [4,5]
+           4 -> [3]
+           5 -> [5])
+    st' = finishKarr st
+
+gaussElim :: Integral a => Vector (Vector a) -> Vector (Vector a)
+gaussElim mat = foldl (\mat i -> elim i mat
+                      ) mat [0..Vec.length mat-1]
+  where
+    elim n mat = case switchZero n mat of
+      Nothing -> mat
+      Just nmat -> foldl (\mat i -> if i==n
+                                    then mat
+                                    else mkZero n i mat
+                         ) nmat [0..Vec.length mat-1]
+    switchZero n mat = if mat!n!n==0
+                       then case Vec.findIndex (\ln -> ln!n/=0) mat of
+                             Just nonNullIdx -> Just $ mat//[(n,mat!nonNullIdx)
+                                                            ,(nonNullIdx,mat!n)]
+                             Nothing -> Nothing
+                       else Just mat
+    mkZero n m mat = let elN = mat!n!n
+                         elM = mat!m!n
+                         f = lcm elN elM
+                     in if elM==0
+                        then mat
+                        else mat//[(m,mat!m^*(f `div` elM) ^-^ mat!n^*(f `div` elN))]
