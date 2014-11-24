926a5b3776602d2da3f3204f9789a5d04021534b
Henning Günther <guenther@forsyte.at>
2014-11-24 14:52:42 +0100

    Nondeterministic functions may not be declared as pure
    Otherwise LLVM will assume that each call yields the same result

--- test/benchmarks.h
+++ test/benchmarks.h
@@ -3,9 +3,9 @@
 #if defined(HCTIGAR)
 void assert(bool);
 void assume(bool);
-int __nondet_int() __attribute__((pure));
-long __nondet_long() __attribute__((pure));
-bool __nondet_bool() __attribute__((pure));
+int __nondet_int();
+long __nondet_long();
+bool __nondet_bool();
 
 #define NONDET_INT(name) int name = __nondet_int()
 #define NONDET_BOOL(name) bool name = __nondet_bool()
