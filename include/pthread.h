7e6fd725cf26938b8962a0163f27e06eea881dc5
Henning Günther <guenther@forsyte.at>
2015-04-08 17:01:00 +0200

    Add simple example

---
+++ include/pthread.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  int id;
+} pthread_t;
+
+typedef struct {
+  void* data;
+} pthread_attr_t;
+
+typedef struct {
+  int id;
+} pthread_mutex_t;
+
+typedef struct {
+  void* data;
+} pthread_mutexattr_t;
+
+// Pthread functions
+
+int pthread_create(pthread_t* pthread,const pthread_attr_t* attr,
+                   void *(*start_routine) (void*),void* arg);
+
+int pthread_yield(void);
+
+int pthread_join(pthread_t* pthread,void** retval);
+
+// Mutex functions
+
+int pthread_mutex_init(pthread_mutex_t *__restrict__ mutex,
+                      const pthread_mutexattr_t *__restrict__ attr);
+
+int pthread_mutex_lock(pthread_mutex_t *mutex);
+
+int pthread_mutex_unlock(pthread_mutex_t *mutex);
+
+#define pthread_join(thread,retval) pthread_join(&thread,retval)
+
+#ifdef __cplusplus
+}
+#endif
---
+++ test/parallel/ring.cpp
@@ -0,0 +1,87 @@
+/*
+ * Ring
+ * ====
+ *
+ *  Lock-free inter-thread ring buffer.
+ *
+ *  *tags*: data structures, C++98
+ *
+ * Description
+ * -----------
+ *
+ *  This program is an example of a lock-free inter-thread ring buffer.
+ *  Verification should proceed without detecting any safety violation.
+ *
+ * Verification
+ * ------------
+ *
+ *       $ divine compile --llvm ring.cpp
+ *       $ divine verify -p assert ring.bc -d
+ *       $ divine verify -p deadlock ring.bc -d
+ *
+ * Execution
+ * ---------
+ *
+ *       $ clang++ -lpthread -o ring.exe ring.cpp
+ *       $ ./ring.exe
+ */
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <assert.h>
+
+template< typename T, int size >
+struct Ring {
+    volatile int reader;
+    T q[ size ];
+    volatile int writer;
+
+    void enqueue( T x ) {
+        while ( (writer + 1) % size == reader ); // full; need to wait
+        q[ writer ] = x;
+        writer = (writer + 1) % size;
+    }
+
+    T dequeue() {
+        T x = q[ reader ];
+        reader = (reader + 1) % size;
+        return x;
+    }
+
+    bool empty() {
+        return reader == writer;
+    }
+
+    Ring() : reader( 0 ), writer( 0 ) {}
+};
+
+typedef Ring< int, 8 > R;
+
+void * reader( void * in ) {
+    R * volatile r = (R *) in;
+    for ( int i = 0; i < 10; ++i ) {
+        while ( r->empty() );
+        int j = r->dequeue();
+        assert( i+10 == j );
+    }
+
+    assert( r->empty() );
+    return 0;
+}
+
+void * writer( void * in) {
+    R * volatile r = (R *) in;
+    for ( int i = 0; i < 10; ++i )
+        r->enqueue( i+10 );
+    return 0;
+}
+
+int main() {
+    R r;
+    pthread_t p1, p2;
+    pthread_create(&p1, NULL, reader, &r);
+    pthread_create(&p2, NULL, writer, &r);
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+    return 0;
+}
