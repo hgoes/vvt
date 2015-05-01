d1bc7a54dd82f9efa3ffac8b3e5aa6440dc8f2dd
Henning Günther <guenther@forsyte.at>
2015-05-01 15:43:29 +0200

    Add support for more datatypes

--- include/vvt.h
+++ include/vvt.h
@@ -4,7 +4,9 @@
 #include <stdbool.h>
 
 extern int __nondet_int();
+extern unsigned int __nondet_uint();
 extern size_t __nondet_size();
+extern bool __nondet_bool();
 
 extern void __yield(int loc);
 extern bool __act(void *(*thread) (void *),int loc,...);
