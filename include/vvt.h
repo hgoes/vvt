077796d785d85ce28a8e18d1e4e4b777bc3335d2
Henning Günther <guenther@forsyte.at>
2015-10-29 09:57:38 +0100

    More include files

--- include/vvt.h
+++ include/vvt.h
@@ -9,6 +9,7 @@ extern "C" {
 
   extern int __nondet_int();
   extern unsigned int __nondet_uint();
+  extern long __nondet_long();
   extern size_t __nondet_size();
   extern bool __nondet_bool();
 
